9c3620b576dac70227209a1782c5a3dd2f8fcacf
khwang1964 <khwang1964@gmail.com>
2026-02-25 14:23:01 +0800

    the second commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v1{3, 9, 1, 7, 5, 10, 6, 8, 2, 4};
+
+    cout << "Orginal Vector: ";
+    for (auto num: v1) 
+	cout << num << " ";
+    cout << endl;
+
+    sort(v1.begin(), v1.end(), greater<>());
+
+    cout << "Sorted Vector: ";
+    for (auto num: v1) 
+        cout << num << " ";
+    cout << endl;
+
+    sort(v1.begin(), v1.end());
+
+    cout << "Sorted Vector: ";
+    for (auto num: v1)
+        cout << num << " ";
+    cout << endl;
+    
+    return 0;
+}
